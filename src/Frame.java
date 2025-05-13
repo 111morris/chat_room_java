e70c5e242ae6e2ab09ccfea00f2ad647294fca5c
mulandi <mulandimorris1@gmail.com>
2025-05-13 23:56:44 +0300

    dvpkg

--- src/Frame.java
+++ src/Frame.java
@@ -4,7 +4,8 @@ public class Frame  {
   static void main(String[] args) {
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    frame.setVisible(true);
+    frame.setSize(500,500);
 
   }
 }
