94ac167bdabacf70c356d4e14abdbf938afd3152
mulandi <mulandimorris1@gmail.com>
2025-05-15 00:06:01 +0300

    dvpkg

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SplashScreen {
   private JFrame frame;
@@ -6,7 +7,14 @@ public class SplashScreen {
   private JTextField serverPortField;
 
   public SplashScreen(){
+    frame = new JFrame("Chatroom Setup");
+    frame.setSize(300,200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(new GridLayout(4,2,10,10));
 
+
+
+    frame.setVisible(true);
   }
 
 }
