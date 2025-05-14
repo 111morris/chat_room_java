0ba53827c32a2c572e6b9e31d9a5e2c2ef4462bc
mulandi <mulandimorris1@gmail.com>
2025-05-14 23:41:19 +0300

    dvpkg

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -4,6 +4,12 @@ public class Dashboard {
   private JFrame frame;
   public  Dashboard(){
     frame = new JFrame("Chatroom Dashboard");
+    frame.setSize(400,400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    initializeUI();
+  }
+  private void initializeUI(){
 
   }
   public void show() {
