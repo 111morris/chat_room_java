36b2693fca20e753ac9fa1ef394a1a584f201c43
mulandi <mulandimorris1@gmail.com>
2025-05-17 09:14:40 +0300

    dvpkg

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -41,25 +41,7 @@ public class Dashboard {
     panel.add(inputPanel, BorderLayout.SOUTH);
     frame.add(panel);
 
-    panel.setLayout(new BorderLayout());
-
-    //chat area
-    JTextPane chatArea = new JTextPane();
-    chatArea.setEditable(false);
-    panel.add(new JScrollPane(chatArea), BorderLayout.CENTER);
-
-    //this will be the input area
-    JPanel inputPanel = new JPanel();
-    inputPanel.setLayout(new BorderLayout());
-
-    JTextField inputField = new JTextField();
-    JButton sendButton = new JButton("Send");
-
-    inputPanel.add(inputField, BorderLayout.CENTER);
-    inputPanel.add(sendButton, BorderLayout.EAST);
-
-    panel.add(inputPanel, BorderLayout.SOUTH);
-    frame.add(panel);
+   
   }
 
   public void show() {
