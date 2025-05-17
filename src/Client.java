04e011ca0a5b73274f289988bc8243b749985183
mulandi <mulandimorris1@gmail.com>
2025-05-17 09:30:44 +0300

    dvpkg

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -6,7 +8,12 @@ public class Client {
   private Socket socket;
   private BufferedReader in;
   private PrintWriter out;
-  
+
+  public Client(String host, int port) throws IOException{
+    socket = new Socket(host, port);
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintWriter(socket.getOutputStream(), true);
+  }
 
 
   public static void main(String[] args) {
