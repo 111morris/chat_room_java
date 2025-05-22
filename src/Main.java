9043ade4b5bf88a8559fab559763d26a753c2554
mulandi <mulandimorris1@gmail.com>
2025-05-22 20:01:54 +0300

    dvpkg

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    //start the server automatic
+    //the server starts automatic
     new Thread(() -> {
       Server server = new Server(9999);
       server.run();
