9a95adf7eedd147b5bf2aac46014e76dad98a37c
mulandi <mulandimorris1@gmail.com>
2025-05-23 23:25:03 +0300

    dvpkg

---
+++ src/cli/Dashboard.java
@@ -0,0 +1,26 @@
+package cli;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+  private JTextField chatArea;
+  private JTextField messageField;
+  private JButton sendButton;
+
+  public Dashboard() {
+    setTitle("Java Chat Room");
+    setSize(400,600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setLayout(new BorderLayout());
+
+    //create a chat display area
+    chatArea = new JTextField();
+    chatArea.setEditable(false);
+    JScrollPane scrollPane = new JScrollPane(chatArea);
+    add(scrollPane, BorderLayout.CENTER);
+
+    //creating the input area
+  }
+}
--- src/cli/Server.java
+++ src/cli/Server.java
@@ -1,6 +1,8 @@
 package cli;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,12 +22,18 @@ public class Server implements Runnable{
    private Socket client;
    private BufferedReader in;
    private PrintWriter out;
+
     public ConnectionHandler(Socket client){
       this.client = client;
     }
     @Override
     public void run(){
-
+      try {
+        out = new PrintWriter(client.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
     }
   }
 }
