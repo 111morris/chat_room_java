0156b4b95fe77cdb2a92552e4de968f1e7407e85
mulandi <mulandimorris1@gmail.com>
2025-06-05 23:41:59 +0300

    dvpkg

--- src/cli/Client.java
+++ src/cli/Client.java
@@ -1,6 +1,7 @@
 package cli;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -8,4 +9,10 @@ public class Client {
   private Socket socket;
   private PrintWriter out;
   private BufferedReader in;
+
+  public Client(String host, int port) {
+    socket = new Socket(host, port);
+    out = new PrintWriter(socket.getOutputStream(), true);
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+  }
 }
