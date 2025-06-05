a254bbdd694f38d33ddc5903fc39ddc99bfad79e
mulandi <mulandimorris1@gmail.com>
2025-06-05 23:36:37 +0300

    dvpkg

--- src/cli/Client.java
+++ src/cli/Client.java
@@ -1,4 +1,11 @@
 package cli;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+  private Socket socket;
+  private PrintWriter out;
+  private BufferedReader in;
 }
