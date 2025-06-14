9f0de4720ee22314be174606c7165a1ee7aab045
mulandi <mulandimorris1@gmail.com>
2025-06-14 21:55:37 +0300

    dvpkg

--- src/cli/Dashboard.java
+++ src/cli/Dashboard.java
@@ -1,1 +1,10 @@
 package cli;
+
+import javax.swing.*;
+
+public class Dashboard extends JFrame{
+  private  String username;
+  public Dashboard(String username, Client client) {
+    this.username = username;
+  }
+}
