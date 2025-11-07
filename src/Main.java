2d979adde4c967e7aaf9b9e51d4cf87e64bb410a
Mulandi <156342676+111morris@users.noreply.github.com>
2025-11-07 22:28:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
 public class Main {
-  public static void main(String[] args) {
-    //the server starts automatic
-    new Thread(() -> {
-      Server server = new Server(9999);
-      server.run();
-    }).start();
-  }
+  
 }
